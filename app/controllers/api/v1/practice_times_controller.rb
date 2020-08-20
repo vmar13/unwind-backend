49e62ca6a55ebab654f5286a8d494c404af8f3fe
github username <60378440+vmar13@users.noreply.github.com>
2020-08-19 22:04:28 -0400

    filled in users and practicetimes controllers

--- app/controllers/api/v1/practice_times_controller.rb
+++ app/controllers/api/v1/practice_times_controller.rb
@@ -1,22 +1,42 @@
 class PracticeTimesController < ApplicationController
   def index
+    practice_times = PracticeTime.all 
+    render json: practice_times
   end
 
   def show
+    practice_time = PracticeTime.find(params[:id]) 
+    render json: practice_time
   end
 
   def new
+    practice_time = PracticeTime.new 
+    render json: practice_time
   end
 
   def create
+    practice_time = PracticeTime.create(practice_time_params)
+    render json: practice_time
   end
 
   def edit
+    practice_time = PracticeTime.find(params[:id])
+    render json: practice_time
   end
 
   def update
+    practice_time = PracticeTime.update(practice_time_params)
+    render json: practice_time
   end
 
   def destroy
+    practice_time = PracticeTime.find(params[:id])
+    practice_time.destroy 
   end
+
+  private 
+
+  def practice_time_params 
+    params.require(:practice_time).permit(:date, :time, :favorite_id)
+  end 
 end
