b2b729bf229fd8865f04811796ef05a4b4908fde
github username <60378440+vmar13@users.noreply.github.com>
2020-08-19 23:26:24 -0400

    filled in favorites and breathingtechniques controllers

--- app/controllers/api/v1/breathing_techniques_controller.rb
+++ app/controllers/api/v1/breathing_techniques_controller.rb
@@ -1,13 +1,27 @@
 class BreathingTechniquesController < ApplicationController
   def index
+    breathing_techniques = BreathingTechnique.all 
+    render json: breathing_techniques
   end
 
   def show
+    breathing_technique = BreathingTechnique.find(params[:id])
+    render json: breathing_technique
   end
 
   def new
+    breathing_technique = BreathingTechnique.new 
+    render json: breathing_technique
   end
 
   def create
+    breathing_technique = BreathingTechnique.create(breathing_technique_params)
+    render json: breathing_technique
   end
+
+  private 
+
+  def breathing_technique_params
+    params.require(:breathing_technique).permit(:name, :step_one, :step_two, :step_three, :step_four)
+  end 
 end
--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,16 +1,30 @@
 class FavoritesController < ApplicationController
   def index
+    favorites = Favorite.all 
+    render json: favorites
   end
 
   def show
+    favorite = Favorite.find(params[:id])
+    render json: favorite 
   end
 
   def new
+    favorite = Favorite.new 
+    render json: favorite
   end
 
   def create
+    favorite = Favorite.create(favorite_params)
+    render json: favorite
   end
 
   def destroy
   end
+
+  private 
+
+  def favorite_params
+    params.require(:favorite).permit(:user_id, :breathing_technique_id)
+  end 
 end
