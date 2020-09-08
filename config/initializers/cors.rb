16e7a5128cdf8d9af6d3756d7c24f4dcdd2a8857
github username <60378440+vmar13@users.noreply.github.com>
2020-09-08 11:12:53 -0400

    fixed cors error

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'example.com'
+    origins '*'
 
     resource '*',
-      headers: 'Access-Control-Allow-Origin': '*',
+      headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
