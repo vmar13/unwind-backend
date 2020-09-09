826f9954de24b1d11682de9dc498888b0c9dc43b
github username <60378440+vmar13@users.noreply.github.com>
2020-09-09 12:03:31 -0400

    added more users paths in resources

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-    resources :users, only: [:create]
+    resources :users, only: [:index, :show, :new, :create]
       post '/login', to: 'auth#create'
       get '/profile', to: 'users#profile'
     resources :breathing_techniques
