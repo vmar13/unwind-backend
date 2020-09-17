e5a8541f6ae9e3c985e90750cebf8b9c3bf51de0
github username <60378440+vmar13@users.noreply.github.com>
2020-09-16 21:35:02 -0400

    setting up serializers and creating tokens

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::UsersController < ApplicationController
-  # skip_before_action :authorized, only: [:create]
+  skip_before_action :authorized, only: [:create]
 
   def index
     users = User.all 
@@ -20,9 +20,9 @@ class Api::V1::UsersController < ApplicationController
     user = User.create(user_params)
     if user.valid?
       @token = encode_token(user_id: user.id)
-    render json: { user: User.new(user), jwt: @token }, status: :created 
+    render json: { user: UserSerializer.new(user), jwt: @token }, status: :created 
     else 
-      render json: { error: 'Failed to create user'}, status: :not_acceptable
+      render json: { error: 'User already exists'}, status: :not_acceptable
     end
   end
 
