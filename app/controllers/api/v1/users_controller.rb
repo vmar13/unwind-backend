c61d164e28ffa0afbf24d27d811f00439a4fb0af
github username <60378440+vmar13@users.noreply.github.com>
2020-09-18 17:45:00 -0400

    fixed users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,15 +1,15 @@
 class Api::V1::UsersController < ApplicationController
-  before_action :authorized, only: [:auto_login]
+  before_action :authorized, only: [:stay_logged_in]
 
-  # def index
-  #   users = User.all 
-  #   render json: users 
-  # end
+  def index
+    users = User.all 
+    render json: users 
+  end
 
-  # def show
-  #   user = User.find(params[:id])
-  #   render json: user 
-  # end
+  def show
+    user = User.find(params[:id])
+    render json: user, include: '**' 
+  end
 
   # def new
   #   user = User.new 
@@ -21,25 +21,26 @@ class Api::V1::UsersController < ApplicationController
     if user.valid?
       payload = {user_id: user.id}
       token = encode_token(payload)
-    render json: { user: user, jwt: token }
+    render json: { user: UserSerializer.new(user), token: token }
     else 
-      render json: { error: user.error.full_messages }, status: :not acceptable
+      render json: { error: 'Invalid username or password.' }
     end
   end
 
   def login 
     user = User.find_by(username: params[:username])
 
-    if user && user.authenticate(params[:password_digest])
+    if user && user.authenticate(params[:password])
       token = encode_token({ user_id: user.id })
-      render json: { user: user, token: token }
+      render json: { user: UserSerializer.new(user), token: token }
     else
       render json: { error: 'Invalid username or password' }
     end
   end
 
-  def auto_login 
-    render json: user
+  def stay_logged_in
+    token = encode_token({ user_id: user.id }) 
+    render json: { user: UserSerializer.new(@user), token: wristband }
   end
 
   private 
