ac6524e74c0ea908e429723bcdc2127d3d4fc90a
github username <60378440+vmar13@users.noreply.github.com>
2020-09-18 15:57:15 -0400

    refactored users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,34 +1,49 @@
 class Api::V1::UsersController < ApplicationController
-  before_action :authorized, only: [:create]
+  before_action :authorized, only: [:auto_login]
 
-  def index
-    users = User.all 
-    render json: users 
-  end
+  # def index
+  #   users = User.all 
+  #   render json: users 
+  # end
 
-  def show
-    user = User.find(params[:id])
-    render json: user 
-  end
+  # def show
+  #   user = User.find(params[:id])
+  #   render json: user 
+  # end
 
-  def new
-    user = User.new 
-    render json: user 
-  end
+  # def new
+  #   user = User.new 
+  #   render json: user 
+  # end
 
   def create
     user = User.create(user_params)
     if user.valid?
-      @token = encode_token(user_id: user.id)
-    render json: { user: UserSerializer.new(user), jwt: @token }, status: :created 
+      token = encode_token(user_id: user.id)
+    render json: { user: user, token: token }
     else 
-      render json: { error: 'User already exists'}, status: :not_acceptable
+      render json: { error: 'Invalid username or password'}
     end
+  end
+
+  def login 
+    user = User.find_by(username: params[:username])
+
+    if user && user.authenticate(params[:password_digest])
+      token = encode_token({ user_id: user.id })
+      render json: { user: user, token: token }
+    else
+      render json: { error: 'Invalid username or password' }
+    end
+  end
+
+  def auto_login 
+    render json: user
   end
 
   private 
 
   def user_params
-    params.require(:user).permit(:username, :password_digest)
+    params.permit(:username, :password_digest)
   end 
 end
