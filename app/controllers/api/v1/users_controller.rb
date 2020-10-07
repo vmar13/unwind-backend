541169d5f13956e5a39355c7102a800002486813
github username <60378440+vmar13@users.noreply.github.com>
2020-10-07 15:20:45 -0400

    fixed before action error

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::UsersController < ApplicationController
-  # wrap_parameters format: [:json, :xml, :url_encoded_form, :multipart_form]
-  # skip_before_action :authorized, only: [:create]
+  skip_before_action :authorized, only: [:create]
 
   def index
     users = User.all 
